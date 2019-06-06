b98634b2fbd61482401e13128ddae62e40248c75
Oscar Shoudt <shoudto@gmail.com>
2019-06-06 01:06:43 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,7 +22,7 @@ class Pokemon
     SQL
     
     pokemon_data = db.execute(sql, id).flatten
-    Pokemon.new(id: pokemon_data[0])
-    binding.pry 
+    Pokemon.new(id: pokemon_data[0], name: pokemon_data[1], type: pokemon_data[2], db: db)
+    #binding.pry 
   end 
 end
